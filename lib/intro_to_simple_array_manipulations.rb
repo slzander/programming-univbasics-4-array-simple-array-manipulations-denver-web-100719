1744bb636a0af28231dced8751734279d30c6a93
Stacey Zander <staceyzander@gmail.com>
2019-08-23 04:07:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,5 +26,19 @@ def using_concat (array, array2)
   array.concat(array2)
 end
 
+def using_insert (array, element)
+  array.insert(element)
+end
+
+def using_uniq (array)
+  array.uniq 
+end
+
+
+  
+  
+  
+  
+
 
 
